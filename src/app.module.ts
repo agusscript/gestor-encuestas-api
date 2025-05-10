829096f2f9098d6bdbaa0e8f815168ef239e37c4
agusscript <agus.sanchez.240@gmail.com>
2025-05-10 19:30:12 -0300

    Add ORM configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { environmentConfig } from './config/environment.config';
+import { ENVIRONMENT } from './config/environment.enum';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { datasourceOptions } from './config/orm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [environmentConfig],
       isGlobal: true,
-      ignoreEnvFile: process.env.NODE_ENV === 'production',
-    })
+      ignoreEnvFile: process.env.NODE_ENV === ENVIRONMENT.PRODUCTION,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...datasourceOptions,
+        autoLoadEntities: true,
+      }),
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
