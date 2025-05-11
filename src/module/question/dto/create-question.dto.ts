607da0c66d7ee6d67b2e0fcf2d0fb4e249121325
agusscript <agus.sanchez.240@gmail.com>
2025-05-11 18:36:44 -0300

    Add options validations in create-question DTO

--- src/module/question/dto/create-question.dto.ts
+++ src/module/question/dto/create-question.dto.ts
@@ -1,5 +1,7 @@
-import { IsEnum, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { ArrayMinSize, IsArray, IsEnum, IsNotEmpty, IsString, MaxLength, MinLength, ValidateIf, ValidateNested } from "class-validator";
 import { QuestionType } from "../enum/question-type.enum";
+import { Type } from "class-transformer";
+import { CreateOptionDto } from "src/module/option/dto/create-option.dto";
 
 export class CreateQuestionDto {
   @IsString()
@@ -11,4 +13,13 @@ export class CreateQuestionDto {
   @IsNotEmpty()
   @IsEnum(QuestionType)
   type: QuestionType;
+
+  @ValidateIf(o => o.type !== QuestionType.OPEN)
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateOptionDto)
+  @ArrayMinSize(1, {
+    message: 'options must contain at least one option'
+  })
+  options?: CreateOptionDto[];
 }
