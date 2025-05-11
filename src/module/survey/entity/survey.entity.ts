7532a317664a5de353b4529c310ad5f6ab27ffe7
agusscript <agus.sanchez.240@gmail.com>
2025-05-11 02:48:06 -0300

    Add survey-question relation

--- src/module/survey/entity/survey.entity.ts
+++ src/module/survey/entity/survey.entity.ts
@@ -2,7 +2,9 @@ import {
   Column,
   Entity,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from "typeorm";
+import { Question } from "src/module/question/entity/question.entity";
 
 @Entity({ name: "survey" })
 export class Survey {
@@ -17,4 +19,9 @@ export class Survey {
 
   @Column({ unique: true })
   resultId: string;
+
+  @OneToMany(() => Question, q => q.survey, {
+    cascade: true,
+  })
+  questions: Question[];
 }
