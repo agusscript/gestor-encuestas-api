6fdc16b33b53c60a09e76c54750e5189dcc54e2e
agusscript <agus.sanchez.240@gmail.com>
2025-05-11 18:37:25 -0300

    Inject option service in question module

--- src/module/question/question.module.ts
+++ src/module/question/question.module.ts
@@ -4,10 +4,12 @@ import { Question } from "./entity/question.entity";
 import { QuestionService } from "./service/question.service";
 import { QuestionRepository } from "./repository/question.repository";
 import { QuestionMapper } from "./mapper/question.mapper";
+import { OptionModule } from "../option/option.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Question])
+    TypeOrmModule.forFeature([Question]),
+    OptionModule,
   ],
   controllers: [],
   providers: [
