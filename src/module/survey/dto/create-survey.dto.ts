a2ca1b9aefae085ef0e6d9e50afb6478ef65a8e1
agusscript <agus.sanchez.240@gmail.com>
2025-05-15 10:41:18 -0300

    Add validation to prevent empty questions in create-survey DTO

--- src/module/survey/dto/create-survey.dto.ts
+++ src/module/survey/dto/create-survey.dto.ts
@@ -1,4 +1,4 @@
-import { IsArray, IsNotEmpty, IsString, MaxLength, MinLength, ValidateNested } from "class-validator";
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString, MaxLength, MinLength, ValidateNested } from "class-validator";
 import { CreateQuestionDto } from "src/module/question/dto/create-question.dto";
 import { Type } from "class-transformer";
 
@@ -10,6 +10,9 @@ export class CreateSurveyDto {
   title: string;
 
   @IsArray()
+  @ArrayMinSize(1, {
+    message: "questions must contain at least one question",
+  })
   @ValidateNested({ each: true })
   @Type(() => CreateQuestionDto)
   questions: CreateQuestionDto[];
